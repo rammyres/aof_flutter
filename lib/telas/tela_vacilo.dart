20b20967139b49b24431c51120f34b31aa539f5a
Rammyres Pereira <rammyres@gmail.com>
2023-11-29 12:27:10 -0300

    Alteração da tela_vacilo

--- lib/telas/tela_vacilo.dart
+++ lib/telas/tela_vacilo.dart
@@ -1,5 +1,6 @@
 import 'package:aof_flutter/modelos/vacilo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TelaVacilo extends StatelessWidget {
   final Vacilo vacilo;
@@ -12,8 +13,23 @@ class TelaVacilo extends StatelessWidget {
     required this.aof,
   }) : super(key: key);
 
+  void _copyToClipboard(String textToCopy, BuildContext context) {
+    Clipboard.setData(ClipboardData(text: textToCopy));
+    const snackBar =
+        SnackBar(content: Text('Texto copiado para a área de transferência'));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final content = '''
+O AOF $aof foi devolvido sob a seguinte justificativa.
+
+*$justificativa*
+
+${vacilo.texto}
+''';
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -25,14 +41,25 @@ class TelaVacilo extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text("O AOF $aof foi devolvido sob a seguinte justificativa."),
-            Text(
-              justificativa,
-              style: const TextStyle(fontStyle: FontStyle.italic),
+            SelectableText.rich(
+              TextSpan(
+                text: content,
+                style: const TextStyle(fontSize: 16.0),
+              ),
+              style: const TextStyle(
+                  fontFamily: 'Roboto'), // Define a fonte desejada
             ),
-            Text(vacilo.texto),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                _copyToClipboard(content, context);
+              },
+              child: const Text('Copiar para a Área de Transferência'),
+            ),
           ],
         ),
       ),
